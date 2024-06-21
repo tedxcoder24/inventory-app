7f100ca163e11fd921dbe4d4eb5dba7e39d8878a
chengzhi <chengzhao>
2024-06-21 04:07:41 -0500

    Updated dashboard for more exact data display

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,6 +16,70 @@ use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
+    public function getInventoryStats($from_date, $to_date)
+    {
+        $item_types = ItemType::all();
+        $statuses = Status::all();
+        $data = [];
+
+        foreach ($item_types as $itemType) {
+            $itemTypeData = [
+                'itemType' => $itemType->item_type,
+                'products' => []
+            ];
+        
+            $products = Product::whereHas('items', function ($query) use ($itemType) {
+                $query->where('item_type_id', $itemType->id);
+            })->get();
+        
+            foreach ($products as $product) {
+                $productData = [
+                    'product' => $product->product,
+                    'statuses' => []
+                ];
+        
+                foreach ($statuses as $status) {
+                    $items = Item::where('item_type_id', $itemType->id)
+                        ->where('product_id', $product->id)
+                        ->whereHas('statuses', function ($query) use ($status, $from_date, $to_date) {
+                            $query->where('status_id', $status->id)
+                                  ->whereBetween('date_time', [$from_date, $to_date]);
+                        })
+                        ->get();
+        
+                    $weight = 0;
+                    $count = 0;
+        
+                    foreach ($items as $item) {
+                        $itemWeights = $item->weights()->whereBetween('date_time', [$from_date, $to_date])->get();
+                        foreach ($itemWeights as $itemWeight) {
+                            $weight += $itemWeight->gross_weight;
+                        }
+                        $count++;
+                    }
+        
+                    if ($count > 0) {
+                        $productData['statuses'][] = [
+                            'status' => $status->status,
+                            'weight' => $weight,
+                            'count' => $count
+                        ];
+                    }
+                }
+        
+                if (!empty($productData['statuses'])) {
+                    $itemTypeData['products'][] = $productData;
+                }
+            }
+        
+            if (!empty($itemTypeData['products'])) {
+                $data[] = $itemTypeData;
+            }
+        }
+
+        return $data;
+    }
+
     public function index(Request $request)
     {
         $from_date_time = $request->input('from_date_time');
@@ -24,78 +88,34 @@ class DashboardController extends Controller
         $from_date = isset($from_date_time) ? Carbon::parse($from_date_time) : Carbon::now()->startOfWeek();
         $to_date = isset($to_date_time) ? Carbon::parse($to_date_time) : Carbon::now()->endOfWeek();
 
-        $products = Product::all();
-        $statuses = Status::all();
-        $item_types = ItemType::all()->pluck('item_type')->toArray();
-        $data = [];
+        // Current week
+        $current_week_start = Carbon::now()->startOfWeek();
+        $current_week_end = Carbon::now()->endOfWeek();
 
-        foreach ($products as $product) {
-            $product_data = [
-                'product' => $product->product,
-                'statuses' => [],
-            ];
+        // Previous week
+        $previous_week_start = Carbon::now()->subWeek()->startOfWeek();
+        $previous_week_end = Carbon::now()->subWeek()->endOfWeek();
 
-            foreach ($statuses as $status) {
-                $status_data = [
-                    'weight' => 0,
-                    'count' => 0,
-                ];
+        // Current month
+        $current_month_start = Carbon::now()->startOfMonth();
+        $current_month_end = Carbon::now()->endOfMonth();
 
-                foreach ($item_types as $item_type) {
-                    $status_data[strtolower($item_type)] = 0;
-                }
+        // Previous month
+        $previous_month_start = Carbon::now()->subMonth()->startOfMonth();
+        $previous_month_end = Carbon::now()->subMonth()->endOfMonth();
 
-                $status_changes = ItemStatus::where('status_id', $status->id)
-                    ->whereBetween('date_time', [$from_date, $to_date])
-                    ->whereHas('item', function ($query) use ($product) {
-                        $query->where('product_id', $product->id);
-                    })
-                    ->get();
+        $current_stats_data = $this->getInventoryStats($from_date, $to_date);
+        $current_week_data = $this->getInventoryStats($current_week_start, $current_week_end);
+        $previous_week_data = $this->getInventoryStats($previous_week_start, $previous_week_end);
+        $current_month_data = $this->getInventoryStats($current_month_start, $current_month_end);
+        $previous_month_data = $this->getInventoryStats($previous_month_start, $previous_month_end);
 
-                foreach ($status_changes as $status_change) {
-                    $weight_change = ItemWeight::where('item_id', $status_change->item_id)
-                        ->where('date_time', $status_change->date_time)
-                        ->first();
-
-                    $weight = $weight_change ? $weight_change->gross_weight : 0;
-                    $status_data['weight'] += $weight;
-                    $status_data['count']++;
-
-                    $item = $status_change->item;
-                    $item_type = $item->itemType->item_type;
-                    $status_data[strtolower($item_type)]++;
-                }
-
-                foreach ($item_types as $item_type) {
-                    $count_field = strtolower($item_type);
-                    if ($status_data[$count_field] > 0) {
-                        $data[] = [
-                            'product' => $product->product,
-                            'statuses' => [
-                                $status->status => [
-                                    'weight' => $status_data['weight'],
-                                    'count' => $status_data['count'],
-                                    $count_field => $status_data[$count_field],
-                                ],
-                            ],
-                        ];
-                    }
-                }
-
-                // // Only add status data if there is relevant data
-                // if ($status_data['count'] > 0) {
-                //     $product_data['statuses'][$status->status] = $status_data;
-                // }
-            }
-
-            // if (!empty($product_data['statuses'])) {
-            //     $data[] = $product_data;
-            // }
-        }
-
-
-        return Inertia::render("Dashboard", [
-            'data' => $data,
+        return Inertia::render("Dashboard/Index", [
+            'current_data' => $current_stats_data,
+            'current_week_data' => $current_week_data,
+            'previous_week_data'=> $previous_week_data,
+            'current_month_data'=> $current_month_data,
+            'previous_month_data'=> $previous_month_data,
             'from_date' => $from_date,
             'to_date' => $to_date
         ]);
---
+++ resources/js/Pages/Dashboard/Partials/StatsSection.vue
@@ -0,0 +1,72 @@
+<script setup>
+defineProps({
+    data: { type: Array },
+});
+</script>
+
+<template>
+    <section>
+        <div class="p-4">
+            <div class="flex flex-col gap-6">
+                <div v-for="(item, dataId) in data" :key="dataId">
+                    <div class="flex justify-around flex-wrap gap-6 m-4">
+                        <div v-for="(product, productId) in item.products" :key="productId">
+                            <table class="block overflow-y-auto whitespace-nowrap border border-gray-200">
+                                <thead class="bg-gray-50">
+                                    <tr class="">
+                                        <th scope="col" class="px-6 py-3 text-lg font-medium tracking-wider text-center text-gray-900 uppercase border-r border-b border-gray-200 bg-yellow-200"> {{ product.product }} </th>
+                                        <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-center text-gray-500 uppercase border-r border-b border-gray-200 bg-indigo-200"> grams </th>
+                                        <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-center text-gray-500 uppercase border-r border-b border-gray-200 bg-indigo-200"> lbs </th>
+                                        <th scope="col" class="px-6 py-3 text-xs font-medium tracking-wider text-center text-gray-500 uppercase border-r border-b border-gray-200 bg-indigo-200"> {{ item.itemType }} </th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white">
+                                    <tr v-for="(status, statusId) in product.statuses" :key="statusId">
+                                        <td class="px-6 py-4 whitespace-nowrap border-r border-gray-200 bg-orange-200">
+                                            <div class="flex items-center justify-center">
+                                                <div>
+                                                    <div class="text-sm font-medium text-gray-900">
+                                                        {{ status.status }}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap border-r border-gray-200">
+                                            <div class="flex items-center justify-center">
+                                                <div>
+                                                    <div class="text-sm font-medium text-gray-900">
+                                                        {{ status.weight }}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap border-r border-gray-200">
+                                            <div class="flex items-center justify-center">
+                                                <div>
+                                                    <div class="text-sm font-medium text-gray-900">
+                                                        {{ (status.weight * 0.00220642).toFixed(2) }}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center justify-center">
+                                                <div>
+                                                    <div class="text-sm font-medium text-gray-900">
+                                                        {{ status.count }}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                    <hr />
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
